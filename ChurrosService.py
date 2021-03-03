a09214e68aaae52ee7840584ccc07c078ee32815
Jonas Jakiminski <28489507+Jakiminski@users.noreply.github.com>
2021-03-03 19:33:54 -0300

    1st commit

---
+++ ChurrosService.py
@@ -0,0 +1,32 @@
+import time
+import random
+#from pathlib import Path
+from SMWinservice import SMWinservice 
+
+# CLASSE DE UM SERVIÇO DO WINDOWS SERVICE
+class Churros (SMWinservice):
+	_svc_name_ = 'Churros'
+	_svc_display_name_ = 'Churros Service'
+	_svc_description_ = 'Fornece churros com recheio de doce de leite aos usuários que rodarem esse serviço.'
+
+	def start(self):
+		self.isrunning = True
+		pass
+
+	def stop(self):
+		self.isrunning = False
+		pass
+
+	def main(self):
+		i = 0
+		while self.isrunning:
+			random.seed()
+			x = random.randint(1,100000)
+			Path(f'C:/Users/famil/Desktop/ChurrosService/pasta/{x}_copia.txt').touch()
+			time.sleep(5)
+		pass
+
+# entry point of the module: copy and paste into the new module
+# ensuring you are calling the "parse_command_line" of the new created class
+if __name__ == '__main__':
+	Churros.parse_command_line()
---
+++ SMWinservice.py
@@ -0,0 +1,57 @@
+'''
+1. Instalar Win32 API para Python:  "pip install pywin32"
+2. Abrir SUPER + R e inserir: "services.msc" para visualizar os serviços
+'''
+
+import socket
+import win32serviceutil
+import servicemanager
+import win32serviceutil
+import win32service
+
+# CLASSE BASE EM PYTHON PARA CRIAR UM SERVIÇO DO WINDOWS SERVICE
+class SMWinservice (win32serviceutil.ServiceFramework):
+	_svc_name_ = 'pythonService'
+	_svc_display_name_ = 'Python Service'
+	_svc_description_ = 'Python Service Description'
+
+	@classmethod
+	def parse_command_line(cls):
+		win32serviceutil.HandleCommandLine(cls)
+		pass
+
+	def __init__(self, args):
+		win32serviceutil.ServiceFramework.__init__(self, args)
+		self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
+		socket.setdefaulttimeout(120)
+		pass
+
+	def SvcStop(self):
+		# Chamado quando o serviço for parado
+		self.stop()
+		self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+		win32event.SetEvent(self.hWaitStop)
+		pass
+
+	def SvcDoRun(self):
+		#Chamado quando o serviço for iniciado
+		self.start()
+		servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE, servicemanager.PYS_SERVICE_STARTED, (self._svc_name_, ''))
+		self.main()
+
+	def start(self):
+		#Override to add logic before the start (eg. running condition)
+		pass
+
+	def stop(self):
+		#Override to add logic before the stop (eg. invalidating running condition)
+		pass
+
+	def main(self):
+		#Main class to be ovverridden to add logic
+		pass
+
+# entry point of the module: copy and paste into the new module
+# ensuring you are calling the "parse_command_line" of the new created class
+if __name__ == '__main__':
+	SMWinservice.parse_command_line()

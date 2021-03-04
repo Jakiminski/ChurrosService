317befe3f53f96e65fdf8e40f210fd40b0042363
Jonas Jakiminski <28489507+Jakiminski@users.noreply.github.com>
2021-03-04 12:28:09 -0300

    stable and running
    still misses log file output

--- hash4dup.py
+++ hash4dup.py
@@ -1,82 +1,83 @@
-# dupFinder.py
-import os, sys
+import os # os.chdir(path), os.getcwd(), os.remove(file)
+import sys
 import hashlib
 import posixpath
 import ntpath
 
 def findDup(parentFolder):
-    # Dups in format {hash:[names]}
-    dups = {}
-    for dirName, subdirs, fileList in os.walk(parentFolder):
-        print('Scanning %s...' % dirName)
-        for filename in fileList:
-            # Get the path to the file
-            path = os.path.join(dirName, filename)
-            # Calculate hash
-            file_hash = hashfile(path)
-            # Add or append the file path
-            if file_hash in dups:
-                dups[file_hash].append(path)
-            else:
-                dups[file_hash] = [path]
-    return dups
+	dups = {}
+	for dirName, subdirs, fileList in os.walk(parentFolder):
+		#print('Procurando em \'%s\'' % dirName)
+		for filename in fileList:
+			# pega path do arquivo
+			path = os.path.join(dirName, filename)
+			# calcula hash
+			file_hash = hashfile(path)
+			# add/append filepath
+			if file_hash in dups:
+				dups[file_hash].append(path)
+			else:
+				dups[file_hash] = [path]
+	return dups
 
-     
-# Joins two dictionaries
 def joinDicts(dict1, dict2):
-    for key in dict2.keys():
-        if key in dict1:
-            dict1[key] = dict1[key] + dict2[key]
-        else:
-            dict1[key] = dict2[key]
+	for key in dict2.keys():
+		if key in dict1:
+			dict1[key] = dict1[key] + dict2[key]
+		else:
+			dict1[key] = dict2[key]
 
- 
 def hashfile(path, blocksize = 65536):
-    afile = open(path, 'rb')
-    hasher = hashlib.md5()
-    buf = afile.read(blocksize)
-    while len(buf) > 0:
-        hasher.update(buf)
-        buf = afile.read(blocksize)
-    afile.close()
-    return hasher.hexdigest()
+	afile = open(path, 'rb')
+	hasher = hashlib.md5()
+	buf = afile.read(blocksize)
+	while len(buf) > 0:
+		hasher.update(buf)
+		buf = afile.read(blocksize)
+	afile.close()
+	return hasher.hexdigest()
+
+def allpaths(folder, pathlist = []):
+	for f in os.scandir(folder):
+		if f.is_dir():
+		 	pathlist.append(f.path)
+		 	allpaths(f.path,pathlist)
+	return pathlist
 
 def path_leaf(path):
     head, tail = ntpath.split(path)
     return tail or ntpath.basename(head)
 
-def printResults(dict1):
-    results = list(filter(lambda x: len(x) > 1, dict1.values()))
-    file1 = open('C:\\Users\\famil\\Desktop\\Jonas\\SO2\\ChurrosService\\ChurrosLog.txt', 'a+')
-    if len(results) > 0:
-        print('Duplicatas Encontradas: ', end = '\n')
-        for result in results:
-            i = 0
-            for subresult in result:
-                print('\t\t%s %i' % (subresult, i))
-                
-                if i>0:
+def results(dict1):
+	results = list(filter(lambda x: len(x) > 1, dict1.values()))
+	logfile = open('C:/Users/famil/Desktop/Jonas/SO2/ChurrosService/DeleteLog.txt', 'a+')
+	if len(results) > 0:
+		#print('Duplicatas descobertas...')
+		for result in results:
+			i = 0
+			#print('{}: {}'.format(i+1,result))
+			for subresult in result:
+				#print('{}:\t{}'.format(i, subresult))
+				if i>0:
+					logfile.write('%s\r\n' % path_leaf(subresult))
+					pathname = subresult.replace(os.sep, ntpath.sep)
+					#print('{}:\t{}'.format(i, subresult))
+					if os.path.exists(pathname):
+						os.remove(pathname)
+				i += 1
+		logfile.close()
+	else:
+		#print('Não havia arquivos repetidos.')
 
-                    file1.write('%s\r\n' % path_leaf(subresult))
-                    getPath = subresult.replace(os.sep, ntpath.sep)
-                    os.remove(getPath)
-                i += 1
-            print('___________________')
-        file1.close
-    else:
-        print('Não há duplicatas.')
+if __name__ == '__main__':
+	# lista com todas as pastas e subpastas
+	folder = 'C:\\Users\\famil\\Desktop\\Jonas\\SO2\\ChurrosService\\root'
+	folders = allpaths(folder,[folder,])
 
-'''
-def runFinder(folders): 
-    duplicates = {}
-    for f in folders:
-        time.sleep(5)
-        # Iterate the folders given
-        if os.path.exists(f):
-            # Find the duplicated files and append them to the dups
-            joinDicts(dups, findDup(f))
-        else:   
-            print('%s is not a valid path, please verify' % i)
-            sys.exit()
-            printResults(duplicates)
-'''
+	duplicates = {}
+	for f in folders: # Iterate the folders given
+		#print(f)
+		# Find the duplicated files and append them to the dups
+		if os.path.exists(f):
+			joinDicts(duplicates, findDup(f))
+		results(duplicates)
